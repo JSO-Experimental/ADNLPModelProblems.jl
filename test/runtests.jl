2bf000762cb1e8b9c9fb8a6c793f087b18da58f1
tmigot <tangi.migot@gmail.com>
2021-06-16 01:08:26 +0200

    rmv warning

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,19 @@
 using ADNLPModelProblems
 
-n = 31
-for pb in ADNLPModelProblems.problems
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_jump(n=$n)"))
-end
+using Suppressor, Test
 
-for pb in ADNLPModelProblems.problems_no_jump
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
+@suppress begin
+  n = 31
+  for pb in ADNLPModelProblems.problems
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_jump(n=$n)"))
+  end
+
+  for pb in ADNLPModelProblems.problems_no_jump
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
+      eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
+  end
 end
