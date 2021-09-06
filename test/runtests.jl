42a7191851457d7032c0b19fc5a47f9d8c36d2a0
tmigot <tangi.migot@gmail.com>
2021-09-06 17:42:53 +0200

    rmv suppressor

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,41 +1,37 @@
-using ADNLPModelProblems, NLPModels
+using ADNLPModelProblems, NLPModels, Test
 
-using Suppressor, Test
+for pb in ADNLPModelProblems.problems
+  n = ADNLPModelProblems.default_nvar
+  eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
+  eval(Meta.parse("ADNLPModelProblems.$(pb)_forward(n=$n)"))
+  eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
+  eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
+  if !(pb in ADNLPModelProblems.problems_no_jump)
+    eval(Meta.parse("ADNLPModelProblems.$(pb)_jump(n=$n)"))
+  end
 
-@suppress begin
-  for pb in ADNLPModelProblems.problems
-    n = ADNLPModelProblems.default_nvar
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_forward(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_reverse(n=$n)"))
-    eval(Meta.parse("ADNLPModelProblems.$(pb)_zygote(n=$n)"))
-    if !(pb in ADNLPModelProblems.problems_no_jump)
-      eval(Meta.parse("ADNLPModelProblems.$(pb)_jump(n=$n)"))
-    end
+  n, m = eval(
+    Meta.parse("ADNLPModelProblems.get_$(pb)_meta(n=$(2 * ADNLPModelProblems.default_nvar))"),
+  )
+  meta_pb = eval(Meta.parse("ADNLPModelProblems.$(pb)_meta"))
+  if meta_pb[:variable_size]
+    @test n != meta_pb[:nvar]
+  else
+    @test n == meta_pb[:nvar]
+  end
+  if meta_pb[:variable_con_size]
+    @test m != meta_pb[:ncon]
+  else
+    @test m == meta_pb[:ncon]
+  end
 
-    n, m = eval(
-      Meta.parse("ADNLPModelProblems.get_$(pb)_meta(n=$(2 * ADNLPModelProblems.default_nvar))"),
-    )
-    meta_pb = eval(Meta.parse("ADNLPModelProblems.$(pb)_meta"))
-    if meta_pb[:variable_size]
-      @test n != meta_pb[:nvar]
-    else
-      @test n == meta_pb[:nvar]
-    end
-    if meta_pb[:variable_con_size]
-      @test m != meta_pb[:ncon]
-    else
-      @test m == meta_pb[:ncon]
-    end
-
-    for T in [Float32, Float64]
-      nlp = eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(type=$(Val(T)))"))
-      x0 = nlp.meta.x0
-      @test eltype(x0) == T
+  for T in [Float32, Float64]
+    nlp = eval(Meta.parse("ADNLPModelProblems.$(pb)_autodiff(type=$(Val(T)))"))
+    x0 = nlp.meta.x0
+    @test eltype(x0) == T
+    @test typeof(obj(nlp, x0)) == T
+    if nlp.meta.ncon > 0
       @test typeof(obj(nlp, x0)) == T
-      if nlp.meta.ncon > 0
-        @test typeof(obj(nlp, x0)) == T
-      end
     end
   end
 end
